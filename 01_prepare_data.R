84203833bd77c8a706733b5ae8a85d27995195b6
Philéas Condemine <phileas.condemine@gmail.com>
2022-02-25 15:37:19 +0100

    add script to generate data

---
+++ 01_prepare_data.R
@@ -0,0 +1,20 @@
+library(data.table)
+data = fread("data/raw/french_tweets.csv",encoding="UTF-8")
+data$id = sapply(1:nrow(data),function(x)paste(sample(c(LETTERS,1:10),10,replace = T),collapse=""))
+setkey(data,id)
+all_ids = data$id
+sub_ids = sample(all_ids, 100)
+sub = data[J(sub_ids)]
+
+sapply(1:nrow(sub),function(i){
+  one_id = sub$id[i]
+  one_text = sub$text[i]
+  writeLines(one_text,file.path("data","documents",paste0(one_id,".txt")))
+})
+
+url_to_files = sub[,.(id)]
+url_to_files[,url:=sprintf("https://raw.githubusercontent.com/phileas-condemine/superannotate_sample/main/documents/%s.txt",id)]
+
+fwrite(url_to_files,"data/url_to_documents_on_github.csv")
+
+
